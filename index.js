7219c22f488f772a41b5d388d48d42592b39fdf0
beloved-processor-5901 <naqashmalik139@gmail.com>
2018-09-27 01:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,19 +13,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-
-function sayHiToGrandma(string)
-{
-  if (string == 'hello')
-    {
-    return "I can't hear you!"
-    }
-  else if (string == 'HELLO')
-    {
-      return "YES INDEED!"
-    }
-  else if (string == "I love you, Grandma.")
-  {
-      return "I love you, too."
-  }
-}
